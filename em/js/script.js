2f26b7b9137c78bf440e93c4d45ba5235c5aa995
Samuel Mun <samuelmun2000@gmail.com>
2025-04-03 21:06:01 -0700

    big changes

---
+++ em/js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggler = document.querySelector('.custom-toggler');
+    const mobileMenu = document.getElementById('mobileMenu');
+    const overlay = document.getElementById('mobileMenuOverlay');
+    const closeBtn = document.getElementById('closeMobileMenu');
+
+    function openMenu() {
+      mobileMenu.classList.add('show');
+      overlay.classList.add('show');
+      document.body.classList.add('no-scroll');
+    }
+
+    function closeMenu() {
+      mobileMenu.classList.remove('show');
+      overlay.classList.remove('show');
+      document.body.classList.remove('no-scroll');
+    }
+
+    toggler.addEventListener('click', openMenu);
+    closeBtn.addEventListener('click', closeMenu);
+    overlay.addEventListener('click', closeMenu);
+  });
